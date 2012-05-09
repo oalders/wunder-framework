67d61558398b6db7833bf49472584922683b87d3
Olaf Alders <olaf@wundersolutions.com>
2012-05-09 15:08:59 -0400

    rollover_day is not relevant to the Billing::Prorate object

--- lib/Wunder/Framework/Billing/Prorate.pm
+++ lib/Wunder/Framework/Billing/Prorate.pm
@@ -6,7 +6,6 @@ has 'amount'       => ( isa => 'Num',      is => 'ro', required => 1 );
 has 'months'       => ( isa => 'Num',      is => 'ro', required => 1 );
 has 'monthly_rate' => ( isa => 'Num',      is => 'ro', required => 1 );
 has 'next_payment' => ( isa => 'DateTime', is => 'ro', required => 1 );
-has 'rollover_day' => ( isa => 'Num',      is => 'ro', required => 1 );
 has 'start_date'   => ( isa => 'DateTime', is => 'ro', required => 1 );
 
 1;
