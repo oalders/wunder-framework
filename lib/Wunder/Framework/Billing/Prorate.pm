620d2718fa0e176f1c7fa81030f49be181a2e719
Olaf Alders <olaf@wundersolutions.com>
2012-05-10 21:57:12 -0400

    Adds Pod to Wunder::Framework::Billing::Prorate

--- lib/Wunder/Framework/Billing/Prorate.pm
+++ lib/Wunder/Framework/Billing/Prorate.pm
@@ -2,6 +2,13 @@ package Wunder::Framework::Billing::Prorate;
 
 use Moose;
 
+=head2 DESCRIPTION
+
+This object doesn't do anything other than hold some values and enforce some
+type constraints.
+
+=cut
+
 has 'amount'       => ( isa => 'Num',      is => 'ro', required => 1 );
 has 'months'       => ( isa => 'Num',      is => 'ro', required => 1 );
 has 'monthly_rate' => ( isa => 'Num',      is => 'ro', required => 1 );
