95a4cbc0033c1993126caf8b1412ac3390a3505c
Olaf Alders <olaf@wundersolutions.com>
2012-06-04 22:01:36 -0400

    Adds script to dump config files

---
+++ tools/dump_conf.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/env perl
+
+use Modern::Perl;
+use Data::Printer;
+use Find::Lib '../lib';
+use Wunder::Framework::Bundle;
+
+=head1 DESCRIPTION
+
+Print out full config for this stream.
+
+=cut
+
+my $wf = Wunder::Framework::Bundle->new;
+print p $wf->config; # STDOUT
+
+=head1 AUTHOR
+
+    Olaf Alders
+    CPAN ID: OALDERS
+    WunderCounter.com
+    olaf@wundersolutions.com
+    http://www.wundercounter.com
+
+=head1 COPYRIGHT
+
+This program is free software; you can redistribute
+it and/or modify it under the same terms as Perl itself.
+
+=cut
