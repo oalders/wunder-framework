8d74b57b951f841c0a14379b5e7b82c25819936e
Olaf Alders <olaf@wundersolutions.com>
2013-06-25 22:10:13 -0500

    Delete a lot of unused stuff.

--- install/cpan.sh
+++
@@ -1,2 +0,0 @@
-Devel::Modlist
-Geo::Coder::GoogleMaps
--- sys-tools/autobundle.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# create a CPAN autobundle and then push all of the snapshot bundles to
-# a backup folder
-
-cpan -a
-cp  /root/.cpan/Bundle/Snapshot* output/cpan_bundles/
