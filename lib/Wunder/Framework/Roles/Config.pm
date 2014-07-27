e34bc3258a3a5bc54ca299d71b818851032539c6
Olaf Alders <olaf@wundersolutions.com>
2014-07-27 22:01:33 +0000

    Minor clean up to Config role.

--- lib/Wunder/Framework/Roles/Config.pm
+++ lib/Wunder/Framework/Roles/Config.pm
@@ -1,45 +1,11 @@
 package Wunder::Framework::Roles::Config;
 
-use Moose::Role;
-use Carp qw( croak );
+use strict;
+use warnings;
+
 use Config::General;
-use Cwd;
-use Find::Lib;
 use Hash::Merge qw( merge );
-use Modern::Perl;
-
-=head1 SYNOPSIS
-
-The various roles required for deploying with config files.
-
-=head2 config
-
-Return config file object for this staging stream (db info).  The file
-should be found here:
-
-/home/co/$stream/$site/conf/$stream/base.cfg
-
-A global config file may be placed at:
-
-/home/co/$stream/$site/conf/global.cfg
-
-This file should contain any elements which are common to all config files or
-any elements which should serve as defaults.  These settings can be
-overridden in any of the base.cfg files
-
-Local config files may be placed at:
-
-/home/co/$stream/$site/conf/$stream/local.cfg
-
-These would generally be files which should not be under version control
-as they would be used to differentiate configs between load balanced
-machines.
-
-This method will return an empty HASHREF if no config files are found.  It
-will not die (or even warn) as there may be perfectly valid reasons for
-using the framework without initialized config files.
-
-=cut
+use Moose::Role;
 
 has 'config' => (
     is      => 'rw',
@@ -95,6 +61,37 @@ sub _build_config_base {
     return $self->path . '/conf/';
 
 }
+
+=head1 SYNOPSIS
+
+The various roles required for deploying with config files.
+
+=head2 config
+
+Return config file object for this staging stream (db info).  The file
+should be found here:
+
+/home/co/$stream/$site/conf/$stream/base.cfg
+
+A global config file may be placed at:
+
+/home/co/$stream/$site/conf/global.cfg
+
+This file should contain any elements which are common to all config files or
+any elements which should serve as defaults.  These settings can be
+overridden in any of the base.cfg files
+
+Local config files may be placed at:
+
+/home/co/$stream/$site/conf/$stream/local.cfg
+
+These would generally be files which should not be under version control
+as they would be used to differentiate configs between load balanced
+machines.
+
+This method will return an empty HASHREF if no config files are found.  It
+will not die (or even warn) as there may be perfectly valid reasons for
+using the framework without initialized config files.
 
 =head1 AUTHOR
 
